c7e7a4d41868beae258d21e4ec5470067449cef0
Taylor Brown <taylorjbrown1234@gmail.com>
2018-02-15 20:50:48 -0500

    Removed Comments

--- src/main/scala/SongStats.scala
+++ src/main/scala/SongStats.scala
@@ -13,18 +13,13 @@ object sparkTop40Search extends p5Parser{
 	  val sc = new SparkContext("local[8]", "First Spark App")
       try{
       val data = sc.textFile("data/top40.txt").map(combinFinder(_))
-      //
-      // Where 5 a. is accomplishe:
-      //
+
       val wordsToExclude = List("", "THE","YOU","I","A","ME","TO","OF","MY","IN","AND","ON","IT","YOUR","BE","IS","FOR","I*M")
       val songsWordOccurance = data.flatMap{ case (artist,song,numOneFlag)=> song.replaceAll("""[\p{Punct}&&[^.|*]]""","").split(" ")}
       .map{ case (word) => if(!wordsToExclude.contains(word)){word} else {(" ")}}
       .filter(!_.contains(" "))
 	  .map{ wordOccurance => (wordOccurance,1)}.reduceByKey(_+_).sortBy(-_._2)
-	  //
-	  // Where 5 a. is printed out, 
-	  // feel free to increase the amount of songs printed:
-	  //
+
     songsWordOccurance.foreach(println)
          val topWords15 = songsWordOccurance.take(15)
 
@@ -38,13 +33,6 @@ object sparkTop40Search extends p5Parser{
         val artistWithMost1 = artistCountAt1Spot.take(15)
      
 
-       
-       
-		//probablityOfFreqWord.foreach(println)
-
-
-	  
-        //new
 
         val num1 = data.filter(_._3 == 1).map{case (artist, song, numOneFlag) => (song,1)}.reduceByKey(_+_).sortBy(-_._2).count.toDouble
          
@@ -71,17 +59,6 @@ object sparkTop40Search extends p5Parser{
 			.filter(_._1.split(" ").contains(word)).count()
 			
 			val tup = (word,((num1SongandWord/num1).toDouble*100))} yield tup
-
-		/*justMapWordsSong.foreach(println)										
-		justMapNum1SongWord.foreach(println)
-
-		val combine = justMapNum1SongWord.merged(justMapWordsSong)({case ((word,count1),(_,count2)) => (word, (count1/count2))})
-		combine.foreach(println)*/
-		
-       //val probabilityOf1 = justMap.map{ case(word,count) => (word, (/count.toDouble)) }
-       //println(songsContainLove.toDouble/num1.count.toDouble)
-
-       //val countOfLove = justMap.map{ case("LOVE",count) => count }.first()
 
        val songFreqWords = data.flatMap{ case (artist,song,numOneFlag)=> song.replaceAll("""[\p{Punct}&&[^.|*]]""","").split(" ")}
        .filter(!_.contains(" "))
@@ -121,10 +98,6 @@ object sparkTop40Search extends p5Parser{
        println("-----------------------------------------------")
        println(" ")
        println(" ")
-       println(" ")
-       println("NOT IN ASSIGNMENT, BUT FOUND:")
-       println(" ")
-       println(" ")
        println("-----------------------------------------------")
        println("Top 15 Most Probable Frequent Words In % Of Top 40 Song Titles: ")
        println("_________________________________________________")
@@ -134,12 +107,6 @@ object sparkTop40Search extends p5Parser{
        println("________________________________________________")
        		probablityOfSongTop.foreach(println)
        println("-----------------------------------------------")
-
-       
-        
-		//probablityOfSongTop.foreach(println)
-         //songAt1Spot.foreach(println)
-	     //println(totalSongs)*
 
          }catch{ case e: Exception => sc.stop()}
             sc.stop()
@@ -153,56 +120,3 @@ object sparkTop40Search extends p5Parser{
       		}
     }
 }
-      //val excludeWord = values.map{ case (word) => if(!wordsToExclude.contains(word)){word}}
-      //val noEmpties =excludeWord.collect(_.isEmpty)
-      	//.filterNot(_.equals((" ",1)))
-      //.reduceByKey(_+_).sortBy(-_._2)
-      //val excludeWord = word.map{ case (word)=> if (!wordsToExclude.contains(word)){(word,1)}} 
-      /*val songWords = for{ song <- values
-                          words = song.replaceAll("""[\p{Punct}&&[^.|*]]""","").trim.split(" ").filterNot(wordsToExclude.contains(_))}yield words.reduce*/
-      //val mapCounts = values.flatMap(song => (song ,1))
-      //val wordCounts = songWords.groupBy(identity).mapValues(_.size)
-      /* = for{ song<- songWords
-          counts = song.groupBy(identity).map{ song_.size)}yield counts*/
-
-        /*def filterLine(line:String):String={   
-          val lineFinalIndex = line.length()-1
-          val removingIndex = line.indexOfSlice(", '")+1 
-          val tokens = line.substring(removingIndex,lineFinalIndex).replaceAll("""[\p{Punct}&&[^.|,|*|'||\|/|-]]""", "")
-          tokens
-     }*/
-     /*trait Flatten[M] extends (M => Seq[(String,String,Int)]){
-     	def apply(m : M) : Seq[(String,String,Int)]
-     }
-
-	 implicit def flattenParseResult[T]
-	  (implicit flattenT : Flatten[T]) = new Flatten[ParseResult[T]]{
-	 	def apply(p: ParseResult[T]) = (p map flattenT) getorElse Nil
-	 }     
-	 def flatten[P](p : P)(implicit flatten : Flatten[P]) = flatten(p)
-     
-     def flatten(res:Any): Seq[(String,String,Int)] = res match{
-     	case x:(String,String,Int) => Seq[_]
-     }*/  
-		
-      
-
-
-       /*val keyWords = for{line <- Source.fromFile("top40.txt").getLines
-                              words = filterLine(line)
-                              finalParse = top40Parser.fullTup(new CharSequenceReader (words))} yield finalParse*/
-       //keyWords.foreach(println)
-       //val top40DataSet = sc.parallelize(keyWords)
-     
-    /*def mostFreq (data: RDD[Tuple3[String,String,Int]]): hashMap(String,Int){
-          val wordsToExclude = List("THE","YOU","I","A","ME","TO","OF","MY","IN","AND","ON","IT","YOUR","BE","IS","FOR","I*M")
-    } */    
-
-    // P() = C(num1 n word) / c(num1)
-    // P() = c(num1 n word) / c(word)
-
-
-
-
-//probablity given the word twilight will be 100% since there is only 
-//one song that contains the word twilight and it was a number 1 hit
